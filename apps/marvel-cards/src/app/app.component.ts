97d4720f72e4c8928e617be2432bb1b67bc50800
Marcio Junior <marciofmjr@hotmail.com>
2022-02-19 15:55:31 -0300

    fix project files using eslint autofix

--- apps/marvel-cards/src/app/app.component.ts
+++ apps/marvel-cards/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'angular-marvel-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  title = 'marvel-cards';
+export class AppComponent implements OnInit {
+  title = "marvel-cards";
+  teste = "abc";
+  lista = [ 1, 2, 3, 4 ]
+
+  obj = {
+    teste: true,
+    'outra-pro': false,
+    outraProp: true,
+    nana: false,
+  }
+
+  anotherObj = {
+    a: true,
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.')
+  }
+  outra(): void {
+    this.testeFn(1, 2)
+  }
+  testeFn(someParameter : number, outra:number):void {
+    console.log(someParameter, outra)
+    this.lista.map(item => {
+      console.log(item)
+    })
+
+    if (someParameter == 3) {
+      console.log(false)
+    } else{
+      console.log(true)
+    }
+
+  }
 }
--- apps/marvel-cards/src/app/app.module.ts
+++ apps/marvel-cards/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppComponent } from './app.component'
+import { NxWelcomeComponent } from './nx-welcome.component'
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [ AppComponent, NxWelcomeComponent ],
+  imports: [ BrowserModule ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule {}
