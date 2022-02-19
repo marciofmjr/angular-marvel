33586adbefc616f778f3acc82c6b3ed963ea04cb
Marcio Junior <marciofmjr@hotmail.com>
2022-02-19 12:03:49 -0300

    init project

---
+++ apps/marvel-cards/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-marvel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'marvel-cards';
+}
