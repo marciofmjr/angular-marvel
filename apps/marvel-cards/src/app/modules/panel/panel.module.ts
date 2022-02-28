30836ded3b163c5744d44d81fb92e89a3d44aff1
Marcio Junior <marciofmjr@hotmail.com>
2022-02-28 16:16:09 -0300

    :star:  styles panel and sidebar container

--- apps/marvel-cards/src/app/modules/panel/panel.module.ts
+++ apps/marvel-cards/src/app/modules/panel/panel.module.ts
@@ -2,14 +2,16 @@ import { PanelRoutingModule } from './panel-routing.module'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { DashboardComponent } from './pages/dashboard/dashboard.component'
-import { PanelComponent } from './panel.component'
+import { PanelComponent } from './panel.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component'
 
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    PanelComponent
+    PanelComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
